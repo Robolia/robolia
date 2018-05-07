89a7c929c646cb436681ea6935ff4ebdd4fcd88f
Rafael Soares <rafaeltravel88@gmail.com>
2018-05-07 23:11:17 +0200

    Fix redirect in home controller

--- lib/game_room_web/controllers/home_controller.ex
+++ lib/game_room_web/controllers/home_controller.ex
@@ -1,5 +1,14 @@
 defmodule GameRoomWeb.HomeController do
   use GameRoomWeb, :controller
 
-  def index(conn, _params), do: conn |> redirect(to: matches_path(conn, :index))
+  def index(conn, _params) do
+    case current_user(conn) do
+      nil ->
+        conn |> render("index.html", current_user: nil)
+      _ ->
+        conn |> redirect(to: matches_path(conn, :index))
+    end
+  end
+
+  defp current_user(conn), do: get_session(conn, :current_user)
 end
